a093bd73b664441cfe25c591dbffb29df10d1cd7
lewapek <lewapek@gmail.com>
2018-10-28 14:45:04 +0100

    build script update

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ ARCHITECTURE=`lscpu | grep Architecture | awk '{print $2}'`
 echo "Found architecture: ${ARCHITECTURE}"
 
 VERSION=`cat version`
+DOCKERFILE_LOCATION="Dockerfile"
 
 case "${ARCHITECTURE}" in
     "x86_64" )
@@ -12,10 +13,12 @@ case "${ARCHITECTURE}" in
     "armv6l" )
         echo "Architecture matches 'armv6l'"
         VERSION="${VERSION}-armv6l"
+        DOCKERFILE_LOCATION="Dockerfile_ARM"
         ;;
     "armv7l" )
         echo "Architecture matches 'armv7l'"
         VERSION="${VERSION}-armv7l"
+        DOCKERFILE_LOCATION="Dockerfile_ARM"
         ;;
     "asd"    )
         echo "Could not match any architecture"
@@ -25,4 +28,4 @@ esac
 
 echo "Building version ${VERSION}"
 
-docker build -t lewap/http-to-influx-writer:${VERSION} .
+docker build -f ${DOCKERFILE_LOCATION} -t lewap/http-to-influx-writer:${VERSION} .
